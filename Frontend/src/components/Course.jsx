4032ab4fadb18dae686432ff9a7ec9a3bfcbb3f0
Chetan1921 <149550764+Chetan1921@users.noreply.github.com>
2024-08-13 04:24:52 +0530

    Update Course.jsx

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -9,7 +9,7 @@ function Course() {
   useEffect(() => {
     const getBooks = async () => {
       try {
-        const response = await axios.get("https://bookstore12-5.onrender.com/api/book/");
+        const response = await axios.get("https://bookstoreapp-6v8p.onrender.com/api/book/");
         console.log(response.data.books);
         setBooks(response.data.books); /// check the response
       } catch (err) {
