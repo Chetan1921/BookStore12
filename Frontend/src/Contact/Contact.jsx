33cc5deb33cb87794ad3302071f84cb9c054c7a8
Chetan1921 <149550764+Chetan1921@users.noreply.github.com>
2024-08-13 23:03:47 +0530

    Update Contact.jsx

--- Frontend/src/Contact/Contact.jsx
+++ Frontend/src/Contact/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
       message,
     };
     try {
-      await axios.post("https://bookstore1234-c9ga.onrender.com/user/Contact", contactInfo);
+      await axios.post("https://bookstoreapp123.onrender.com/user/Contact", contactInfo);
       toast.success("Thanks for contacting us!");
       setTimeout(() => {
         toast.success("We will contact you soon!");
