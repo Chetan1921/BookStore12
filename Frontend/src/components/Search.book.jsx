9751957f6dcecfd68ad57d4e15129da7b73be9b7
Chetan1921 <149550764+Chetan1921@users.noreply.github.com>
2024-08-13 22:41:40 +0530

    Update Search.book.jsx

--- Frontend/src/components/Search.book.jsx
+++ Frontend/src/components/Search.book.jsx
@@ -12,7 +12,7 @@ const BookSearch = () => {
     e.preventDefault();
     try {
       const response = await axios.get(
-        `http://localhost:5001/api/book/search?query=${query}`
+        `https://bookstore1234-c9ga.onrender.com/api/book/search?query=${query}`
       );
       console.log(response.data); // Check the response
       setResults(response.data);
